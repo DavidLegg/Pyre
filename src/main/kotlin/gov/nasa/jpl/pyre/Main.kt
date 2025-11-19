b59641540983086907ea3137acae22f6a560b97a
David Legg <david.legg@jpl.nasa.gov>
2025-11-18 18:31:58 -0800

    Partially debug scheduling example

--- src/main/kotlin/gov/nasa/jpl/pyre/Main.kt
+++ src/main/kotlin/gov/nasa/jpl/pyre/Main.kt
@@ -6,5 +6,6 @@ fun main(args: Array<String>) {
 //    gov.nasa.jpl.pyre.examples.orbit.simpleMain(args)
 //    gov.nasa.jpl.pyre.examples.orbit.csvMain(args)
 //    gov.nasa.jpl.pyre.examples.orbiter.power.main(args)
-    gov.nasa.jpl.pyre.examples.sequencing.main(args)
+//    gov.nasa.jpl.pyre.examples.sequencing.main(args)
+    gov.nasa.jpl.pyre.examples.scheduling.main(args)
 }
