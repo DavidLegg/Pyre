e8890b35564788ff3ec050f9f2a2f52f28f78f84
David Legg <david.legg@jpl.nasa.gov>
2026-03-25 17:50:52 -0700

    Document how to repro a fuzz testing failure

--- src/test/kotlin/gov/nasa/jpl/pyre/incremental/foundation/IncrementalSimulatorTest.kt
+++ src/test/kotlin/gov/nasa/jpl/pyre/incremental/foundation/IncrementalSimulatorTest.kt
@@ -1,5 +1,6 @@
 package gov.nasa.jpl.pyre.incremental.foundation
 
+import gov.nasa.jpl.pyre.examples.scheduling.GroundedActivity
 import gov.nasa.jpl.pyre.foundation.plans.Activity
 import gov.nasa.jpl.pyre.foundation.plans.ActivityActions.call
 import gov.nasa.jpl.pyre.foundation.plans.ActivityActions.spawn
@@ -46,6 +47,7 @@ import gov.nasa.jpl.pyre.incremental.IncrementalSimulatorOperations.minus
 import gov.nasa.jpl.pyre.incremental.IncrementalSimulatorOperations.move
 import gov.nasa.jpl.pyre.incremental.IncrementalSimulatorOperations.plus
 import gov.nasa.jpl.pyre.incremental.IncrementalSimulatorOperations.remove
+import gov.nasa.jpl.pyre.incremental.IncrementalSimulatorOperations.unaryPlus
 import gov.nasa.jpl.pyre.incremental.KernelIncrementalSimulator
 import gov.nasa.jpl.pyre.incremental.PlanEdits
 import gov.nasa.jpl.pyre.incremental.foundation.TestModel.*
@@ -655,6 +657,168 @@ class IncrementalSimulatorTest {
         test(startTime = day1, endTime = day3, incon = checkpoint1)
         val checkpoint2 = simulator1.save(day2)
         test(startTime = day2, endTime = day3, incon = checkpoint2)
+    }
+
+    /** Reproduce the observed NPE-from-continuation by calling a fuzz test with the observed failing seed. */
+    @Test
+    fun `repro by seed`() {
+        `random plan edits conform to fundamental incremental sim guarantee`(1)
+    }
+
+    /**
+     * Reproduce the observed NPE-from-continuation by taking each action the fuzz test took.
+     * This test was produced with the help of repro_incremental_sim_fuzz_test.py
+     */
+    @Test
+    fun `repro directly`() {
+        var incon: Checkpoint<TestModel>
+        var inconTime: Instant
+        println("Building initial plan")
+        var tester = test(
+            GroundedActivity(Instant.parse("2025-01-01T00:01:07.691928Z"), Name("483838683711"), SpawnChild(child=AddJob(seed=18))),
+            GroundedActivity(Instant.parse("2025-01-01T11:56:27.940281Z"), Name("349568391770"), ReportStandaloneCounter(id="2528")),
+            GroundedActivity(Instant.parse("2025-01-01T17:47:50.377810Z"), Name("325113240617"), AddJob(seed=10)),
+            GroundedActivity(Instant.parse("2025-01-01T18:22:21.404800Z"), Name("256221124709"), SpawnChild(child=AddJob(seed=7))),
+            GroundedActivity(Instant.parse("2025-01-01T12:55:17.666966Z"), Name("386191371839"), SpawnChild(child=SpawnChildren(id="SC-2010"))),
+            GroundedActivity(Instant.parse("2025-01-01T06:14:17.630151Z"), Name("119816780415"), SpawnChild(child=SpawnChild(child=ReportStandaloneCounter(id="1509")))),
+            GroundedActivity(Instant.parse("2025-01-01T14:35:19.140819Z"), Name("295304103185"), SpawnChildren(id="SC-5669")),
+            GroundedActivity(Instant.parse("2025-01-01T16:49:36.294154Z"), Name("352428644765"), AddJob(seed=26)),
+            GroundedActivity(Instant.parse("2025-01-01T03:24:52.038696Z"), Name("151805940528"), SetIntegrand(number=0.3003489828134367)),
+            GroundedActivity(Instant.parse("2025-01-01T17:25:50.550098Z"), Name("290184593803"), IncrementStandaloneCounter(number=-7)),
+            GroundedActivity(Instant.parse("2025-01-01T17:32:01.723516Z"), Name("160634307677"), SetStandaloneCounter(number=82)),
+            GroundedActivity(Instant.parse("2025-01-01T14:33:43.856263Z"), Name("519742823152"), SpawnChildren(id="SC-5567")),
+            GroundedActivity(Instant.parse("2025-01-01T15:37:33.148581Z"), Name("132745857368"), SetStandaloneCounter(number=44)),
+            GroundedActivity(Instant.parse("2025-01-01T16:09:19.708097Z"), Name("418018393883"), SpawnChild(child=IncrementStandaloneCounter(number=0))),
+            GroundedActivity(Instant.parse("2025-01-01T15:20:43.997321Z"), Name("784316188272"), SpawnChild(child=SetStandaloneCounter(number=0))),
+            GroundedActivity(Instant.parse("2025-01-01T11:26:23.975439Z"), Name("784455777462"), SetStandaloneCounter(number=74)),
+            GroundedActivity(Instant.parse("2025-01-01T16:36:33.638122Z"), Name("844547961313"), ReportStandaloneCounter(id="6642")),
+            GroundedActivity(Instant.parse("2025-01-01T00:17:47.198723Z"), Name("442643708271"), IncrementStandaloneCounter(number=-8)),
+            GroundedActivity(Instant.parse("2025-01-01T16:51:25.005002Z"), Name("600679260745"), SpawnChild(child=ReportStandaloneCounter(id="9281")))
+        )
+        println("Running round 1 of edits")
+        tester.run(
+            add(GroundedActivity(Instant.parse("2025-01-01T07:06:12.722870Z"), Name("110112859732"), SetStandaloneCounter(number=68)))
+                    + move(GroundedActivity(Instant.parse("2025-01-01T16:36:33.638122Z"), Name("844547961313"), ReportStandaloneCounter(id="6642")) to Instant.parse("2025-01-01T16:44:07.956141Z"))
+        )
+        println("Running round 2 of edits")
+        tester.run(
+            add(GroundedActivity(Instant.parse("2025-01-01T03:54:55.334418Z"), Name("687465476078"), SetDerivationSource(number=-1)))
+                    + add(GroundedActivity(Instant.parse("2025-01-01T12:15:35.390755Z"), Name("985293002262"), SpawnChildren(id="SC-5577")))
+        )
+        println("Running round 3 of edits")
+        println("Doing a save/restore cycle")
+        inconTime = Instant.parse("2025-01-01T14:03:24.102133Z")
+        incon = tester.save(inconTime)
+        tester = test(startTime = inconTime, endTime = inconTime + 1.days, incon = incon, activities = listOf(
+            GroundedActivity(Instant.parse("2025-01-02T00:56:21.146768Z"), Name("122387740366"), SpawnChild(child=ReportStandaloneCounter(id="8021"))),
+            GroundedActivity(Instant.parse("2025-01-01T16:36:33.112779Z"), Name("656410423044"), SetIntegrand(number=-0.005422981314735953)),
+            GroundedActivity(Instant.parse("2025-01-02T12:07:56.711566Z"), Name("116308768193"), IncrementStandaloneCounter(number=5)),
+            GroundedActivity(Instant.parse("2025-01-01T23:34:21.435726Z"), Name("854383325353"), ReportStandaloneCounter(id="9803")),
+            GroundedActivity(Instant.parse("2025-01-01T15:41:57.517017Z"), Name("367145481076"), SetIntegrand(number=-0.1315897050805397)),
+            GroundedActivity(Instant.parse("2025-01-01T19:24:01.502171Z"), Name("681130853720"), IncrementStandaloneCounter(number=4)),
+            GroundedActivity(Instant.parse("2025-01-02T08:30:09.616369Z"), Name("546552665989"), SetIntegrand(number=0.7846684320909298)),
+            GroundedActivity(Instant.parse("2025-01-02T12:20:26.505776Z"), Name("882595588967"), IncrementStandaloneCounter(number=-9)),
+            GroundedActivity(Instant.parse("2025-01-02T13:42:38.295769Z"), Name("680316375282"), SetStandaloneCounter(number=18)),
+            GroundedActivity(Instant.parse("2025-01-01T22:03:43.765440Z"), Name("480582196310"), IncrementStandaloneCounter(number=-3)),
+            GroundedActivity(Instant.parse("2025-01-01T14:04:07.413226Z"), Name("959883837961"), AddJob(seed=26)),
+            GroundedActivity(Instant.parse("2025-01-02T01:51:08.439063Z"), Name("189728688931"), SpawnChild(child=SpawnChildren(id="SC-7085"))),
+            GroundedActivity(Instant.parse("2025-01-02T01:26:09.659204Z"), Name("684012532568"), SpawnChild(child=ReportStandaloneCounter(id="9790"))),
+            GroundedActivity(Instant.parse("2025-01-01T14:31:02.001784Z"), Name("181136543870"), SetStandaloneCounter(number=32)),
+            GroundedActivity(Instant.parse("2025-01-02T00:56:15.768292Z"), Name("210552885869"), SetStandaloneCounter(number=8)),
+            GroundedActivity(Instant.parse("2025-01-02T05:08:58.235039Z"), Name("488336183103"), IncrementStandaloneCounter(number=7)),
+            GroundedActivity(Instant.parse("2025-01-02T11:37:59.691216Z"), Name("865203221181"), AddJob(seed=3)),
+            GroundedActivity(Instant.parse("2025-01-01T18:25:15.600199Z"), Name("622722202468"), ReportStandaloneCounter(id="7144")),
+            GroundedActivity(Instant.parse("2025-01-02T04:16:41.443806Z"), Name("998273946340"), SpawnChild(child=IncrementStandaloneCounter(number=-2))),
+            GroundedActivity(Instant.parse("2025-01-01T18:37:04.659401Z"), Name("265098842607"), ReportStandaloneCounter(id="3830")),
+            GroundedActivity(Instant.parse("2025-01-02T10:40:37.894889Z"), Name("943996879570"), SpawnChildren(id="SC-5543")),
+            GroundedActivity(Instant.parse("2025-01-01T17:13:58.006706Z"), Name("968937714079"), SetIntegrand(number=-0.07147104319954622)),
+            GroundedActivity(Instant.parse("2025-01-01T22:41:22.825002Z"), Name("677711445927"), AddJob(seed=16)),
+            GroundedActivity(Instant.parse("2025-01-02T02:18:15.306623Z"), Name("341198935927"), SpawnChildren(id="SC-1840")),
+            GroundedActivity(Instant.parse("2025-01-02T02:54:44.902225Z"), Name("439935907428"), SetDerivationSource(number=0)),
+            GroundedActivity(Instant.parse("2025-01-02T09:39:47.030566Z"), Name("436998965330"), SetIntegrand(number=-0.3726086441772669)),
+            GroundedActivity(Instant.parse("2025-01-01T22:44:13.748951Z"), Name("534703930220"), ReportStandaloneCounter(id="6546")),
+            GroundedActivity(Instant.parse("2025-01-01T23:42:20.495141Z"), Name("932607094647"), AddJob(seed=25)),
+            GroundedActivity(Instant.parse("2025-01-02T10:05:38.150683Z"), Name("666790583887"), SetDerivationSource(number=8)),
+            GroundedActivity(Instant.parse("2025-01-01T20:45:46.716011Z"), Name("147057969579"), IncrementStandaloneCounter(number=2)),
+            GroundedActivity(Instant.parse("2025-01-01T19:18:56.531388Z"), Name("107406119393"), SpawnChildren(id="SC-8935")),
+            GroundedActivity(Instant.parse("2025-01-02T11:45:12.968529Z"), Name("861253450155"), SetStandaloneCounter(number=22)),
+            GroundedActivity(Instant.parse("2025-01-02T13:53:43.729041Z"), Name("129091353286"), SpawnChildren(id="SC-8494")),
+            GroundedActivity(Instant.parse("2025-01-02T05:29:49.292856Z"), Name("592592861091"), SetIntegrand(number=-0.9643550474445832)),
+            GroundedActivity(Instant.parse("2025-01-02T12:22:36.601295Z"), Name("612519236186"), SetStandaloneCounter(number=30)),
+            GroundedActivity(Instant.parse("2025-01-01T17:59:03.339889Z"), Name("580420789010"), SpawnChildren(id="SC-5238")),
+            GroundedActivity(Instant.parse("2025-01-02T02:41:48.845907Z"), Name("911024632947"), SpawnChild(child=SetStandaloneCounter(number=22))),
+            GroundedActivity(Instant.parse("2025-01-01T23:17:02.626969Z"), Name("134602793307"), SetIntegrand(number=-0.7438690970847854)),
+            GroundedActivity(Instant.parse("2025-01-01T21:47:49.845015Z"), Name("497706077150"), ReportStandaloneCounter(id="7726")),
+            GroundedActivity(Instant.parse("2025-01-02T07:46:26.081105Z"), Name("387345215400"), SpawnChild(child=SpawnChildren(id="SC-5039"))),
+            GroundedActivity(Instant.parse("2025-01-02T08:01:08.709364Z"), Name("314433926652"), ReportStandaloneCounter(id="7112")),
+            GroundedActivity(Instant.parse("2025-01-02T12:26:45.662973Z"), Name("972023483898"), IncrementStandaloneCounter(number=0)),
+            GroundedActivity(Instant.parse("2025-01-01T16:15:50.692131Z"), Name("495684308847"), SpawnChildren(id="SC-8837")),
+            GroundedActivity(Instant.parse("2025-01-02T06:47:55.804413Z"), Name("171357431931"), SpawnChildren(id="SC-3164")),
+            GroundedActivity(Instant.parse("2025-01-01T17:05:35.490912Z"), Name("198225535728"), ReportStandaloneCounter(id="6494")),
+            GroundedActivity(Instant.parse("2025-01-02T11:46:48.491029Z"), Name("202036406628"), SetDerivationSource(number=-5)),
+            GroundedActivity(Instant.parse("2025-01-01T15:12:14.407716Z"), Name("949078935600"), IncrementStandaloneCounter(number=4)),
+            GroundedActivity(Instant.parse("2025-01-02T08:41:40.078547Z"), Name("928774283102"), SetDerivationSource(number=-3)),
+            GroundedActivity(Instant.parse("2025-01-02T13:29:14.732373Z"), Name("651137161823"), ReportStandaloneCounter(id="3317")),
+            GroundedActivity(Instant.parse("2025-01-02T08:42:40.644658Z"), Name("227748433408"), SpawnChildren(id="SC-7451")),
+            GroundedActivity(Instant.parse("2025-01-01T15:29:42.555916Z"), Name("415238998136"), ReportStandaloneCounter(id="5551")),
+            GroundedActivity(Instant.parse("2025-01-02T02:04:07.067893Z"), Name("809147861308"), SetDerivationSource(number=-2)),
+            GroundedActivity(Instant.parse("2025-01-02T03:25:17.556899Z"), Name("624207202651"), SpawnChild(child=SetStandaloneCounter(number=-6))),
+            GroundedActivity(Instant.parse("2025-01-01T19:07:12.430603Z"), Name("494082806349"), AddJob(seed=27)),
+            GroundedActivity(Instant.parse("2025-01-01T14:38:58.378861Z"), Name("487068682942"), SetIntegrand(number=0.41563792321014703)),
+            GroundedActivity(Instant.parse("2025-01-01T15:31:55.357057Z"), Name("643876860936"), SpawnChildren(id="SC-7504")),
+            GroundedActivity(Instant.parse("2025-01-01T14:25:53.308841Z"), Name("597730179485"), SetIntegrand(number=-0.28776053395534684)),
+            GroundedActivity(Instant.parse("2025-01-01T19:32:20.085239Z"), Name("561115011909"), IncrementStandaloneCounter(number=-10)),
+            GroundedActivity(Instant.parse("2025-01-01T22:25:39.313440Z"), Name("446589105918"), ReportStandaloneCounter(id="1481")),
+            GroundedActivity(Instant.parse("2025-01-01T21:34:01.597275Z"), Name("923505151174"), SpawnChild(child=AddJob(seed=4))),
+            GroundedActivity(Instant.parse("2025-01-02T06:01:28.419695Z"), Name("670572579461"), SpawnChild(child=ReportStandaloneCounter(id="8901"))),
+            GroundedActivity(Instant.parse("2025-01-02T12:26:09.578804Z"), Name("966521764981"), SetDerivationSource(number=-10)),
+            GroundedActivity(Instant.parse("2025-01-01T17:30:05.396217Z"), Name("636702302039"), SetIntegrand(number=-0.46732398262812014))
+        ))
+        println("Save/restore cycle complete")
+        tester.run(
+            remove(GroundedActivity(Instant.parse("2025-01-01T15:31:55.357057Z"), Name("643876860936"), SpawnChildren(id="SC-7504")))
+                    + move(GroundedActivity(Instant.parse("2025-01-02T12:26:09.578804Z"), Name("966521764981"), SetDerivationSource(number=-10)) to Instant.parse("2025-01-02T12:20:29.737532Z"))
+                    + add(GroundedActivity(Instant.parse("2025-01-02T05:41:04.587522Z"), Name("870621409001"), SetIntegrand(number=-0.63023398009347)))
+                    + move(GroundedActivity(Instant.parse("2025-01-02T04:16:41.443806Z"), Name("998273946340"), SpawnChild(child=IncrementStandaloneCounter(number=-2))) to Instant.parse("2025-01-02T04:23:12.201832Z"))
+                    + remove(GroundedActivity(Instant.parse("2025-01-01T23:34:21.435726Z"), Name("854383325353"), ReportStandaloneCounter(id="9803")))
+                    + remove(GroundedActivity(Instant.parse("2025-01-01T23:42:20.495141Z"), Name("932607094647"), AddJob(seed=25)))
+                    + add(GroundedActivity(Instant.parse("2025-01-02T06:41:03.146383Z"), Name("370126189594"), SetIntegrand(number=-0.1910991369829993)))
+                    + add(GroundedActivity(Instant.parse("2025-01-01T18:16:13.606290Z"), Name("143989872362"), SetDerivationSource(number=9)))
+                    + move(GroundedActivity(Instant.parse("2025-01-02T11:45:12.968529Z"), Name("861253450155"), SetStandaloneCounter(number=22)) to Instant.parse("2025-01-02T11:54:11.533276Z"))
+            + add(GroundedActivity(Instant.parse("2025-01-01T21:12:11.136256Z"), Name("737497575449"), SetDerivationSource(number=7)))
+                    + remove(GroundedActivity(Instant.parse("2025-01-02T08:42:40.644658Z"), Name("227748433408"), SpawnChildren(id="SC-7451")))
+                    + remove(GroundedActivity(Instant.parse("2025-01-02T06:47:55.804413Z"), Name("171357431931"), SpawnChildren(id="SC-3164")))
+                    + add(GroundedActivity(Instant.parse("2025-01-01T22:29:00.344174Z"), Name("196186781863"), ReportStandaloneCounter(id="7360")))
+                    + remove(GroundedActivity(Instant.parse("2025-01-02T01:26:09.659204Z"), Name("684012532568"), SpawnChild(child=ReportStandaloneCounter(id="9790"))))
+                    + remove(GroundedActivity(Instant.parse("2025-01-01T22:25:39.313440Z"), Name("446589105918"), ReportStandaloneCounter(id="1481")))
+                    + remove(GroundedActivity(Instant.parse("2025-01-02T08:30:09.616369Z"), Name("546552665989"), SetIntegrand(number=0.7846684320909298)))
+                    + move(GroundedActivity(Instant.parse("2025-01-02T10:40:37.894889Z"), Name("943996879570"), SpawnChildren(id="SC-5543")) to Instant.parse("2025-01-02T10:43:03.953751Z"))
+                    + add(GroundedActivity(Instant.parse("2025-01-02T10:58:36.293496Z"), Name("403822234439"), SetStandaloneCounter(number=22)))
+                    + remove(GroundedActivity(Instant.parse("2025-01-02T02:54:44.902225Z"), Name("439935907428"), SetDerivationSource(number=0)))
+                    + add(GroundedActivity(Instant.parse("2025-01-02T11:17:55.616765Z"), Name("836056941328"), SetIntegrand(number=-0.5396062570775171)))
+                    + move(GroundedActivity(Instant.parse("2025-01-02T13:29:14.732373Z"), Name("651137161823"), ReportStandaloneCounter(id="3317")) to Instant.parse("2025-01-02T13:35:29.944506Z"))
+                    + remove(GroundedActivity(Instant.parse("2025-01-02T08:41:40.078547Z"), Name("928774283102"), SetDerivationSource(number=-3)))
+                    + remove(GroundedActivity(Instant.parse("2025-01-02T02:18:15.306623Z"), Name("341198935927"), SpawnChildren(id="SC-1840")))
+                    + add(GroundedActivity(Instant.parse("2025-01-01T18:07:57.668070Z"), Name("834467251300"), SetDerivationSource(number=9)))
+                    + add(GroundedActivity(Instant.parse("2025-01-02T12:46:56.775076Z"), Name("662451633014"), SpawnChildren(id="SC-8902")))
+        )
+        println("Repro complete!")
+    }
+
+    /**
+     * The "cut down" version of [`repro directly`], where I removed as much as possible while still reproducing the error.
+     */
+    @Test
+    fun `repro directly -- cut down`() {
+        var tester = test()
+        val inconTime = Instant.parse("2025-01-01T14:03:24.102133Z")
+        val incon = tester.save(inconTime)
+        tester = test(startTime = inconTime, endTime = inconTime + 1.days, incon = incon, activities = listOf(
+            GroundedActivity(Instant.parse("2025-01-02T10:05:38.150683Z"), Name("666790583887"), SetDerivationSource(number=8)),
+        ))
+        tester.run(
+            add(GroundedActivity(Instant.parse("2025-01-01T21:12:11.136256Z"), Name("737497575449"), SetDerivationSource(number=7)))
+        )
     }
 
     /**
