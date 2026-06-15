3d448b7f3927d03efcd060c7bfdee4b761883600
David Legg <david.legg@jpl.nasa.gov>
2026-06-15 16:54:21 -0700

    Discover tutorials automatically

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,12 @@ plugins {
 }
 rootProject.name = "pyre"
 
-include("tutorials:_01_modeling:_01_hello_world")
-include("tutorials:_01_modeling:_02_discrete_resources")
+file("tutorials").listFiles()?.forEach { section ->
+    if (section.isDirectory) {
+        section.listFiles()?.forEach { lesson ->
+            if (lesson.isDirectory) {
+                include("tutorials:${section.name}:${lesson.name}")
+            }
+        }
+    }
+}
