d649429d52a494010e09ef750840f267461ecd94
David Legg <david.legg@jpl.nasa.gov>
2026-06-25 12:04:32 -0700

    WIP - sketch using DuckDB appender

--- src/main/kotlin/gov/nasa/jpl/pyre/examples/orbit/Main.kt
+++ src/main/kotlin/gov/nasa/jpl/pyre/examples/orbit/Main.kt
@@ -1,10 +1,44 @@
 package gov.nasa.jpl.pyre.examples.orbit
 
+import gov.nasa.jpl.pyre.foundation.Simulator
 import gov.nasa.jpl.pyre.general.plans.runStandardPlanSimulation
+import gov.nasa.jpl.pyre.general.reporting.DuckDbReportHandler
+import org.duckdb.DuckDBConnection
+import java.sql.DriverManager
+import kotlin.time.Instant
 
 // This is a simple setup, using default choices for how to run a simulation.
 // Most importantly, it chooses to read and write files for incon, plan, outputs, and fincon,
 // and to use the default CSV event output format.
 fun simpleMain(args: Array<String>) {
     runStandardPlanSimulation(args[0], ::EarthOrbit, EarthOrbit.JSON_FORMAT)
+}
+
+fun main(args: Array<String>) {
+    val channels_file = "test_data/orbit/channels.parquet"
+    val reports_file = "test_data/orbit/reports.parquet"
+    val start = Instant.parse("2030-01-01T00:00:00Z")
+    val end = Instant.parse("2031-01-01T00:00:00Z")
+
+    (DriverManager.getConnection("jdbc:duckdb:") as DuckDBConnection).use { connection ->
+        DuckDbReportHandler(connection, EarthOrbit.JSON_FORMAT).use { handler ->
+            val simulator = Simulator(
+                handler,
+                start,
+                constructModel = ::EarthOrbit
+            )
+            simulator.runUntil(end)
+
+            connection.prepareStatement(
+                """
+            COPY channels TO '$channels_file' (FORMAT parquet)
+        """.trimIndent()
+            ).execute()
+            connection.prepareStatement(
+                """
+            COPY reports TO '$reports_file' (FORMAT parquet)
+        """.trimIndent()
+            ).execute()
+        }
+    }
 }
--- src/main/kotlin/gov/nasa/jpl/pyre/general/reporting/DuckDbReportHandler.kt
+++ src/main/kotlin/gov/nasa/jpl/pyre/general/reporting/DuckDbReportHandler.kt
@@ -3,16 +3,19 @@ package gov.nasa.jpl.pyre.general.reporting
 import gov.nasa.jpl.pyre.foundation.reporting.BaseChannelizedReportHandler
 import gov.nasa.jpl.pyre.foundation.reporting.ChannelReport
 import gov.nasa.jpl.pyre.utilities.Serialization.encodeToString
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toJavaLocalDateTime
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.json.Json
+import org.duckdb.DuckDBAppender
 import org.duckdb.DuckDBConnection
-import java.sql.PreparedStatement
 
 class DuckDbReportHandler(
     private val connection: DuckDBConnection,
     private val json: Json
-) : BaseChannelizedReportHandler() {
-    private val insertChannelStmt: PreparedStatement
-    private val insertReportStmt: PreparedStatement
+) : BaseChannelizedReportHandler(), AutoCloseable {
+    private val channelAppender: DuckDBAppender
+    private val reportAppender: DuckDBAppender
 
     init {
         connection.createStatement()
@@ -26,31 +29,36 @@ class DuckDbReportHandler(
             .execute("""
                 CREATE TABLE IF NOT EXISTS reports (
                     time TIMESTAMP_NS,
-                    FOREIGN KEY channel VARCHAR REFERENCES channels(channel),
-                    data JSON
+                    channel VARCHAR,
+                    data JSON,
+                    FOREIGN KEY (channel) REFERENCES channels(channel)
                 )""".trimIndent())
 
-        insertChannelStmt = checkNotNull(connection.prepareStatement("""
-            INSERT INTO channels (channel, metadata) VALUES (?, ?)
-        """.trimIndent()))
-        insertReportStmt = checkNotNull(connection.prepareStatement("""
-            INSERT INTO reports (time, channel, data) VALUES (?, ?, ?)
-        """.trimIndent()))
+        channelAppender = checkNotNull(connection.createAppender("channels"))
+        reportAppender = checkNotNull(connection.createAppender("reports"))
     }
 
     override fun <T> constructChannel(metadata: ChannelReport.ChannelMetadata<T>): (ChannelReport.ChannelData<T>) -> Unit {
-        insertChannelStmt.setString(1, metadata.channel.toString())
-        insertChannelStmt.setString(2, json.encodeToString(metadata.metadata))
-        insertChannelStmt.executeUpdate()
-        insertChannelStmt.clearParameters()
+        // TODO: Figure out how to append structs directly instead of through serialization like this
+        val channelString = metadata.channel.toString()
+        channelAppender.beginRow()
+        channelAppender.append(channelString)
+        channelAppender.append(json.encodeToString(metadata.metadata))
+        channelAppender.endRow()
+        // Eagerly flush channel appender to avoid foreign key violations in the table.
+        channelAppender.flush()
 
-        // TODO: Batch insert report statements
         return { data ->
-            insertReportStmt.setString(1, data.time.toString())
-            insertReportStmt.setString(2, data.channel.toString())
-            insertReportStmt.setString(3, json.encodeToString(metadata.dataType, data.data))
-            insertReportStmt.execute()
-            insertReportStmt.clearParameters()
+            reportAppender.beginRow()
+            reportAppender.append(data.time.toLocalDateTime(TimeZone.UTC).toJavaLocalDateTime())
+            reportAppender.append(channelString)
+            reportAppender.append(json.encodeToString(metadata.dataType, data.data))
+            reportAppender.endRow()
         }
+    }
+
+    override fun close() {
+        channelAppender.close()
+        reportAppender.close()
     }
 }
