5d42cd63c51269ed9f68dd48abfd413cdf2388f7
David Legg <david.legg@jpl.nasa.gov>
2026-06-25 18:51:30 -0700

    WIP - Testing different output methods
    Overall, parallel CSV writing is far and away the best-performing output strategy for time,
    though the CSV file it produces is over twice the size of the equivalent parquet file.
    
    Full Results:
    
    Serial CSV:
      Average overall time:    11.933098411s
      Average simulation time: 11.926645383s
    
    Serial DuckDB:
      Average overall time:    12.478580975s
      Average simulation time: 12.264746026s
    
    Parallel CSV:
      Average overall time:    5.987792258s
      Average simulation time: 5.982890751s
    
    Parallel DuckDB:
      Average overall time:    9.627564845s
      Average simulation time: 9.479961496s
    
    Output size:
      reports.csv:     281M
      reports.parquet: 117M (~42% of CSV)
    
    Work to go: Try removing the primary key and foreign key constraints in DuckDB to see if that's dramatically slowing us down for some reason.

--- src/main/kotlin/gov/nasa/jpl/pyre/examples/orbit/Main.kt
+++ src/main/kotlin/gov/nasa/jpl/pyre/examples/orbit/Main.kt
@@ -3,12 +3,19 @@ package gov.nasa.jpl.pyre.examples.orbit
 import gov.nasa.jpl.pyre.foundation.Simulator
 import gov.nasa.jpl.pyre.foundation.reporting.ChannelizedReportHandler
 import gov.nasa.jpl.pyre.general.plans.runStandardPlanSimulation
+import gov.nasa.jpl.pyre.general.reporting.CsvReportHandler
+import gov.nasa.jpl.pyre.general.reporting.ParallelReportHandler.Companion.inParallel
 import gov.nasa.jpl.pyre.general.reporting.useParallelReportHandler
 import gov.nasa.jpl.pyre.general.reporting.useReportHandler
-import kotlinx.coroutines.runBlocking
 import org.duckdb.DuckDBConnection
 import java.sql.DriverManager
+import kotlin.io.path.Path
+import kotlin.io.path.div
+import kotlin.io.path.outputStream
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.Instant
+import kotlin.time.measureTime
 
 // This is a simple setup, using default choices for how to run a simulation.
 // Most importantly, it chooses to read and write files for incon, plan, outputs, and fincon,
@@ -18,14 +25,73 @@ fun simpleMain(args: Array<String>) {
 }
 
 fun main(args: Array<String>) {
-    val channels_file = "test_data/orbit/channels.parquet"
-    val reports_file = "test_data/orbit/reports.parquet"
+    val aggregateTestTiming = AggregateTestTiming()
+    // Run 25 of each combination, in random order.
+    // This should roughly average out the effects of JIT "warming"
+    (1..100).shuffled().forEach {
+        aggregateTestTiming.runTest(it)
+    }
+
+    fun List<TestTiming>.printAverage() {
+        val averageOverall = fold(Duration.ZERO) { acc, timing -> acc + timing.overall } / size
+        val averageSimulation = fold(Duration.ZERO) { acc, timing -> acc + timing.simulation } / size
+        println("  Average overall time:    $averageOverall")
+        println("  Average simulation time: $averageSimulation")
+    }
+
+    println("\n\nAggregate results:")
+    aggregateTestTiming.serialCsvTimings.printAverage()
+    aggregateTestTiming.serialDuckDbTimings.printAverage()
+    aggregateTestTiming.parallelCsvTimings.printAverage()
+    aggregateTestTiming.parallelDuckDbTimings.printAverage()
+}
+
+val out_dir = Path("test_data/orbit/")
+
+data class AggregateTestTiming(
+    val serialCsvTimings: MutableList<TestTiming> = mutableListOf(),
+    val parallelCsvTimings: MutableList<TestTiming> = mutableListOf(),
+    val serialDuckDbTimings: MutableList<TestTiming> = mutableListOf(),
+    val parallelDuckDbTimings: MutableList<TestTiming> = mutableListOf(),
+)
+
+data class TestTiming(
+    val overall: Duration,
+    val simulation: Duration,
+)
+
+fun AggregateTestTiming.runTest(index: Int) {
+    val useDuckDb = index % 2 == 1
+    val useParallel = index % 4 < 2
+    val resultList = if (useDuckDb) {
+        if (useParallel) {
+            parallelDuckDbTimings
+        } else {
+            serialDuckDbTimings
+        }
+    } else {
+        if (useParallel) {
+            parallelCsvTimings
+        } else {
+            serialCsvTimings
+        }
+    }
+    resultList += runTest(useDuckDb, useParallel)
+}
+
+fun runTest(useDuckDb: Boolean, useParallel: Boolean): TestTiming {
+    println("--- Test: ${if (useDuckDb) "DuckDB" else "CSV"} ${if (useParallel) "parallel" else "serial"} ---")
+
     val start = Instant.parse("2000-01-01T00:00:00Z")
     val end = Instant.parse("2200-01-01T00:00:00Z")
-    val useParallel = true
+    val parallelString = if (useParallel) "parallel" else "serial"
+    val channels_file = out_dir / "channels.$parallelString.parquet"
+    val reports_file = out_dir / "reports.$parallelString.parquet"
+    val csv_file = out_dir / "reports.$parallelString.csv"
 
-    (DriverManager.getConnection("jdbc:duckdb:") as DuckDBConnection).use { connection ->
-        val runSimulation: (ChannelizedReportHandler) -> Unit = { handler ->
+    var simulationDuration: Duration = 0.seconds
+    val runSimulation: (ChannelizedReportHandler) -> Unit = { handler ->
+        simulationDuration = measureTime {
             val simulator = Simulator(
                 handler,
                 start,
@@ -33,13 +99,35 @@ fun main(args: Array<String>) {
             )
             simulator.runUntil(end)
         }
-        if (useParallel) {
-            connection.useParallelReportHandler(EarthOrbit.JSON_FORMAT, runSimulation)
-        } else {
-            connection.useReportHandler(EarthOrbit.JSON_FORMAT, runSimulation)
-        }
-
-        connection.prepareStatement("COPY channels TO '$channels_file' (FORMAT parquet)").execute()
-        connection.prepareStatement("COPY reports TO '$reports_file' (FORMAT parquet)").execute()
     }
+
+    val overallDuration = measureTime {
+        if (useDuckDb) {
+            (DriverManager.getConnection("jdbc:duckdb:") as DuckDBConnection).use { connection ->
+                if (useParallel) {
+                    connection.useParallelReportHandler(EarthOrbit.JSON_FORMAT, runSimulation)
+                } else {
+                    connection.useReportHandler(EarthOrbit.JSON_FORMAT, runSimulation)
+                }
+
+                // Testing shows that the time to write the parquet file out of memory is negligible.
+                connection.prepareStatement("COPY channels TO '$channels_file' (FORMAT parquet)").execute()
+                connection.prepareStatement("COPY reports TO '$reports_file' (FORMAT parquet)").execute()
+            }
+        } else {
+            csv_file.outputStream().use { outputStream ->
+                CsvReportHandler(outputStream, EarthOrbit.JSON_FORMAT).use {
+                    if (useParallel) {
+                        it.inParallel(runSimulation)
+                    } else {
+                        runSimulation(it)
+                    }
+                }
+            }
+        }
+    }
+
+    println("Overall time:    $overallDuration")
+    println("Simulation time: $simulationDuration")
+    return TestTiming(overallDuration, simulationDuration)
 }
